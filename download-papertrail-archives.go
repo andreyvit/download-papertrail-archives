9f5b0a20c3886bc75195e794ea1c2da0db54cdb6
Andrey Tarantsov <andrey@tarantsov.com>
2020-07-29 13:48:55 +0300

    Date filtering, quiet mode, cli improvements

--- download-papertrail-archives.go
+++ download-papertrail-archives.go
@@ -10,28 +10,36 @@ import (
 	urlpkg "net/url"
 	"os"
 	"path/filepath"
+	"regexp"
 	"sort"
 	"time"
 )
 
-var token = flag.String("token", "", "Papertrail HTTP API token from https://papertrailapp.com/account/profile")
+var (
+	token     = flag.String("token", "", "Papertrail HTTP API token from https://papertrailapp.com/account/profile")
+	outputDir = flag.String("o", ".", "Output directory")
+	timeout   = flag.Duration("timeout", 30*time.Second, "timeout for HTTP operations")
+	quiet     = flag.Bool("q", false, "quiet operation (don't print anything)")
 
-var outputDir = flag.String("o", ".", "Output directory (default: .)")
+	until, since Date
+)
 
 const (
 	archivesEndpoint = "https://papertrailapp.com/api/v1/archives.json"
 )
 
 func main() {
+	flag.Var(&until, "until", "only download logs on or before this date in YYYY-MM-DD format")
+	flag.Var(&since, "since", "only download logs on or after this date in YYYY-MM-DD format")
 	log.SetFlags(0)
 	flag.Parse()
 
 	if *token == "" {
-		log.Fatalf("** Papertrail token required (-token <TOKEN>)")
+		log.Fatalf("** Papertrail token required (-token <TOKEN>), see https://papertrailapp.com/account/profile")
 	}
 
 	client := &http.Client{
-		Timeout: 30 * time.Second,
+		Timeout: *timeout,
 	}
 	headers := http.Header{
 		"X-Papertrail-Token": []string{*token},
@@ -39,9 +47,12 @@ func main() {
 
 	var archives []*Archive
 
+	if !*quiet {
+		log.Printf("Fetching a list of archives...")
+	}
 	err := downloadJSON(archivesEndpoint, headers, client, &archives)
 	if err != nil {
-		log.Fatalf("** Fetching archives.json: %v", err)
+		log.Fatalf("** ERROR: cannot fetch a list of archives: %v", err)
 	}
 
 	sort.Slice(archives, func(i, j int) bool {
@@ -52,39 +63,50 @@ func main() {
 		Existing   int
 		Failed     int
 		Downloaded int
+		Skipped    int
 	}{}
 
 	for _, a := range archives {
 		fn := filepath.Join(*outputDir, a.Filename)
 		url := a.Links.Download.URL
 		if url == "" {
-			log.Printf("No URL: %v", a.Filename)
+			log.Printf("WARNING: Skipping entry with no URL: %v", a.Filename)
 			continue
 		}
 
+		if !since.IsZero() && a.Filename < since.String() {
+			stats.Skipped++
+		}
+		if !until.IsZero() && a.Filename > until.String() {
+			stats.Skipped++
+		}
+
 		if _, err := os.Stat(fn); err == nil {
-			log.Printf("Exists: %v", a.Filename)
 			stats.Existing++
 		} else {
-			log.Printf("Downloading: %v", a.Filename)
+			if !*quiet {
+				log.Printf("Downloading %v...", a.Filename)
+			}
 
 			raw, err := download(url, headers, client)
 			if err != nil {
-				log.Printf("Failed to download %v: %v", a.Filename, err)
+				log.Printf("WARNING: cannot download %v: %v", a.Filename, err)
 				stats.Failed++
 				continue
 			}
 
 			err = ioutil.WriteFile(fn, raw, 0644)
 			if err != nil {
-				log.Fatalf("Failed to save %v: %v", a.Filename, err)
+				log.Fatalf("** ERROR: cannot save %v: %v", a.Filename, err)
 			}
 
 			stats.Downloaded++
 		}
 	}
 
-	log.Printf("%d downloaded, %d failed, %d existing skipped", stats.Downloaded, stats.Failed, stats.Existing)
+	if !*quiet {
+		log.Printf("Done: %d downloaded, %d failed, %d already exist, %d skipped.", stats.Downloaded, stats.Failed, stats.Existing, stats.Skipped)
+	}
 }
 
 type Archive struct {
@@ -133,4 +155,48 @@ func downloadJSON(url string, headers http.Header, client *http.Client, resp int
 	}
 
 	return json.Unmarshal(b, resp)
+}
+
+var (
+	dayRe = regexp.MustCompile(`^(\d\d\d\d)-(\d\d)-(\d\d)$`)
+)
+
+type Date string
+
+func FromTime(tm time.Time) Date {
+	return Date(tm.Format("2006-01-02"))
+}
+func Today() Date {
+	return FromTime(time.Now())
+}
+func Yesterday() Date {
+	return FromTime(time.Now().AddDate(0, 0, -1))
+}
+
+func (v Date) IsZero() bool {
+	return len(v) == 0
+}
+
+func (v Date) String() string {
+	return string(v)
+}
+
+func ParseDate(s string) (Date, error) {
+	switch s {
+	case "tod", "today":
+		return Today(), nil
+	case "yest", "yesterday":
+		return Yesterday(), nil
+	}
+	if m := dayRe.FindStringSubmatch(s); m != nil {
+		return Date(m[0]), nil
+	} else {
+		return Date(""), fmt.Errorf("invalid date: %q", s)
+	}
+}
+
+func (v *Date) Set(s string) error {
+	d, err := ParseDate(s)
+	*v = d
+	return err
 }
